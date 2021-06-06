196e271a8aef53009800dd94440083af94ac8df9
stcYps <stcyps14@gmail.com>
2021-06-07 04:58:37 +0900

    とりあえずがわだけ

---
+++ src/main/kotlin/net/kunmc/lab/compulsorysummons/CompulsorySummonsPlugin.kt
@@ -0,0 +1,9 @@
+package net.kunmc.lab.compulsorysummons
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class CompulsorySummonsPlugin: JavaPlugin() {
+    override fun onEnable() {
+        logger.info("起動しました")
+    }
+}
